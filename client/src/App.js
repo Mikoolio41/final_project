f13dd38fe975a06f53e5ceee3ca00f304aea8854
Mika Bibas <mikabibas@gmail.com>
2022-08-13 16:26:19 +0300

    update server client

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response)
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
+  return (
+    <div>
+      {typeof backendData.users === "undefined" ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => <p key={i}>{user}</p>)
+      )}
+      <h1>hi</h1>
+    </div>
+  );
+}
+
+export default App;
