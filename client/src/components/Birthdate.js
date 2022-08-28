ddf9b2b1f02e600b915d3ba667498337f9f4ae7c
Mika Bibas <mikabibas@gmail.com>
2022-08-28 15:14:02 +0300

    front update

--- client/src/components/Birthdate.js
+++ client/src/components/Birthdate.js
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
-import DatePicker from "react-datepicker";
+import React from "react";
 import "react-datepicker/dist/react-datepicker.css";
+import TextField from "@mui/material/TextField";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 export default function Birthdate() {
-  const [date, setDate] = useState(new Date());
-  const handleChange = (date) => setDate(date);
-
+  const [value, setValue] = React.useState(Date);
+  console.log(value);
   return (
-    <DatePicker className="calendar" selected={date} onChange={handleChange} />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker
+        label="Basic example"
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
   );
 }
--- client/src/containers/Signup.js
+++ client/src/containers/Signup.js
@@ -2,6 +2,8 @@ import React from "react";
 import Birthdate from "../components/Birthdate.js";
 // import { router } from "/server/routes/plans";
 import Gender from "../components/Gender.js";
+import "./UserInputForm.css";
+import { useState } from "react";
 
 // user and password
 // regex
@@ -19,7 +21,7 @@ import Gender from "../components/Gender.js";
 //   console.log(regexEmail.test(address));
 // });
 
-// let regexPass = new RegExp("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,15}$");
+let regexPass = new RegExp("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,15}$");
 
 // let testPass = ["mika345", "Ha489!@-fsdkjf", "Gyhfdkd398402", "fjdsjhO934?$"];
 
@@ -28,106 +30,139 @@ import Gender from "../components/Gender.js";
 // });
 
 function Signup() {
+  const [formData, updateFormData] = React.useState(null);
+  const [pass, setPass] = useState(null);
+  // const [touched, setTouched] = React.useState(false);
+  const handleChange = (e) => {
+    updateFormData({
+      ...formData,
+
+      // Trimming any whitespace
+      [e.target.name]: e.target.value.trim(),
+    });
+  };
+
   return (
-    <div className="form">
-      <div className="title">Please fill out the info below</div>
-      {/* <div className="subtitle">Please insert your info:</div> */}
-      <div className="input-container ic1">
-        <input
-          className="input"
-          type="text"
-          id="first_name"
-          name="first_name"
-          placeholder=" "
-        />
-        <div className="cut"></div>
-        <label className="placeholder" for="first_name">
-          First Name
-        </label>
-      </div>
-      <div className="input-container ic1">
-        <input
-          className="input"
-          type="text"
-          id="last_name"
-          name="last_name"
-          placeholder=" "
-        />
-        <div className="cut"></div>
-        <label className="placeholder" for="last_name">
-          Last Name
-        </label>
-      </div>
-      <div className="input-container ic1">
-        <input
-          className="input"
-          type="text"
-          id="email"
-          name="email"
-          placeholder=" "
-        />
-        <div className="cut"></div>
-        <label className="placeholder" for="email">
-          Email
-        </label>
-      </div>
-      <div className="input-container ic1">
-        <input
-          className="input"
-          type="text"
-          id="password"
-          name="password"
-          placeholder=" "
-        />
-        <div className="cut"></div>
-        <label className="placeholder" for="password">
-          Password
-        </label>
-      </div>
-      <div className="input-container ic1">
-        <input
-          className="input"
-          type="text"
-          id="height"
-          name="height"
-          placeholder=" "
-        />
-        <div className="cut"></div>
-        <label className="placeholder" for="height">
-          Height in cm
-        </label>
-      </div>
-      <div className="input-container ic2">
-        <input
-          type="text"
-          id="weight"
-          className="input"
-          name="weight"
-          placeholder=" "
-        />
-        <div className="cut"></div>
-        <label className="placeholder" for="weight">
-          Weight in kg
-        </label>
-      </div>
-      <div className="input-container ic2">
-        <label className="labelBirthdate" for="birthday">
-          Please enter your birthdate:
-        </label>
-        <Birthdate />
-        <label className="labelGender" for="gender">
-          You identify as:
-        </label>
-        <Gender name="gender" />
-        <label className="goal labelGoal" for="goal">
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(formData);
+      }}
+    >
+      <div className="form">
+        <div className="title">Please fill out the info below</div>
+        {/* <div className="subtitle">Please insert your info:</div> */}
+        <div className="input-container ic1">
+          <input
+            className="input"
+            type="text"
+            id="first_name"
+            name="first_name"
+            placeholder=" "
+            onChange={handleChange}
+          />
+          <div className="cut"></div>
+          <label className="placeholder" htmlFor="first_name">
+            First Name
+          </label>
+        </div>
+        <div className="input-container ic1">
+          <input
+            className="input"
+            type="text"
+            id="last_name"
+            name="last_name"
+            placeholder=" "
+            onChange={handleChange}
+          />
+          <div className="cut"></div>
+          <label className="placeholder" htmlFor="last_name">
+            Last Name
+          </label>
+        </div>
+        <div className="input-container ic1">
+          <input
+            className="input"
+            type="email"
+            id="email"
+            name="email"
+            placeholder=" "
+            onChange={handleChange}
+          />
+          <div className="cut"></div>
+          <label className="placeholder" htmlFor="email">
+            Email
+          </label>
+        </div>
+        <div className="input-container ic1">
+          <input
+            className="input"
+            type="password"
+            id="password"
+            name="password"
+            placeholder=" "
+            onBlur={(e) => {
+              if (e.target.value.match(regexPass) != null) {
+                setPass({ password: e.target.value });
+              } else {
+                alert(`1 Uppercase\n1 Lowercase\n1 Digit\n8-15 Characters`);
+              }
+            }}
+          />
+          <div className="cut"></div>
+          <label className="placeholder" htmlFor="password">
+            Password
+          </label>
+        </div>
+        <div className="input-container ic1">
+          <input
+            className="input"
+            type="text"
+            id="height"
+            name="height"
+            placeholder=" "
+            onChange={handleChange}
+          />
+          <div className="cut"></div>
+          <label className="placeholder" htmlFor="height">
+            Height in cm
+          </label>
+        </div>
+        <div className="input-container ic2">
+          <input
+            type="text"
+            id="weight"
+            className="input"
+            name="weight"
+            placeholder=" "
+            onChange={handleChange}
+          />
+          <div className="cut"></div>
+          <label className="placeholder" htmlFor="weight">
+            Weight in kg
+          </label>
+        </div>
+        <div className="input-container ic2">
+          <label className="labelBirthdate" htmlFor="birthday">
+            Please enter your birthdate:
+          </label>
+          <Birthdate onChange={handleChange} />
+          <div className="input-container ic2">
+            <label className="labelGender" htmlFor="gender">
+              You identify as:
+            </label>
+            <Gender onChange={handleChange} name="gender" />
+          </div>
+          {/* <label className="goal labelGoal" htmlFor="goal">
           What's your training goal?
-        </label>
-        {/* <Goal name="goal" /> */}
-        <button className="submit" type="submit">
-          Submit
-        </button>
+        </label> */}
+          {/* <Goal name="goal" /> */}
+          <button className="submit" type="submit">
+            Submit
+          </button>
+        </div>
       </div>
-    </div>
+    </form>
   );
 }
 
