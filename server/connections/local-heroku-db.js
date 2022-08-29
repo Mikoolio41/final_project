8033d58102e4521ae9942482fe26e5e6129e2970
Mika Bibas <mikabibas@gmail.com>
2022-08-29 23:47:15 +0300

    url

--- server/connections/local-heroku-db.js
+++ server/connections/local-heroku-db.js
@@ -16,13 +16,4 @@ const db = knex({
   },
 });
 
-// db.raw("SELECT 1")
-//   .then(() => {
-//     console.log("PostgreSQL connected");
-//   })
-//   .catch((e) => {
-//     console.log("PostgreSQL not connected");
-//     console.error(e);
-//   });
-
 module.exports = { db };
