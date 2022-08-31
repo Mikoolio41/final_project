68b5300f1b00bbe6923ec90587c9c26a4b58fd31
Mika Bibas <mikabibas@gmail.com>
2022-08-31 19:49:53 +0300

    more components

---
+++ client/src/containers/UserPlan.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Exercise from "../components/Exercise";
+import Image from "../components/Image";
+
+function UserPlan() {
+  return (
+    <div>
+      <Exercise />
+      <Image />
+    </div>
+  );
+}
+
+export default UserPlan;
