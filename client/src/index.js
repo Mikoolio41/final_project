0086d5c0eda98dae23330ca51db65a960f45a8c3
Mika Bibas <mikabibas@gmail.com>
2022-09-24 19:28:03 +0300

    make it better

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./containers/Homepage";
 import Signup from "./containers/Signup";
