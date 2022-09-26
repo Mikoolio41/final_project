1c0747a1a6cfbb8154fea2e611d1abc85e426129
Mika Bibas <mikabibas@gmail.com>
2022-09-26 11:23:31 +0300

    update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const router = require("./routes/plans");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 dotenv.config({ path: "C:/GitHub/final_project/.env" });
 
@@ -15,5 +16,11 @@ app.listen(process.env.PORT || 8080, () => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use("/", express.static(path.resolve(__dirname, "./client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.use("/", router);
