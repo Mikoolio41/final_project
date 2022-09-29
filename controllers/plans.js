4c40807c5084158499b8cf3e536bb9bb6e466bc5
Mika Bibas <mikabibas@gmail.com>
2022-09-29 19:04:47 +0300

    update

--- controllers/plans.js
+++ controllers/plans.js
@@ -108,10 +108,10 @@ const userLogin = async (req, res) => {
     if (result.length == 0) {
       res.send({ msg: "you're not registered" });
     } else if (result.length > 0 && result[0].password === req.body.password) {
-      res.send({ msg: result[0] });
+      res.send({ msg: result[0].password });
       // res.send({ first_name: result[0].first_name, userid: result[0].id });
     } else {
-      res.send({ msg: result[0] });
+      res.send({ msg: result[0].password });
     }
   } catch (error) {
     console.log(error);
