a1f85389e25a3146a8f7470866c0941c6e9047ea
Mika Bibas <mikabibas@gmail.com>
2022-09-29 20:40:58 +0300

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-// import DesktopBreakpoint from "./components/responsive_utilities/desktop_breakpoint";
-// import TabletBreakpoint from "./components/responsive_utilities/tablet_breakpoint";
-// import PhoneBreakpoint from "./components/responsive_utilities/phone_breakpoint";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./containers/Homepage";
+import Signup from "./containers/Signup";
+import Login from "./containers/Login";
+import Profile from "./containers/Profile";
+import Navbar from "./components/Navbar";
+import MyPlans from "./containers/MyPlans";
+import Logout from "./containers/Logout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/register" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/plan" element={<MyPlans />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/logout" element={<Logout />} />
+    </Routes>
+  </BrowserRouter>
+);
