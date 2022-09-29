7d6d96b404019229fa0b089985a1bfff5f57a85a
Mika Bibas <mikabibas@gmail.com>
2022-09-29 20:33:58 +0300

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import DesktopBreakpoint from "./components/responsive_utilities/desktop_breakpoint";
-import TabletBreakpoint from "./components/responsive_utilities/tablet_breakpoint";
-import PhoneBreakpoint from "./components/responsive_utilities/phone_breakpoint";
+// import DesktopBreakpoint from "./components/responsive_utilities/desktop_breakpoint";
+// import TabletBreakpoint from "./components/responsive_utilities/tablet_breakpoint";
+// import PhoneBreakpoint from "./components/responsive_utilities/phone_breakpoint";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <DesktopBreakpoint>
-      <App />
-    </DesktopBreakpoint>
-    <TabletBreakpoint>
-      <App />
-    </TabletBreakpoint>
-    {/* <PhoneBreakpoint>
-      <App />
-    </PhoneBreakpoint> */}
+    <App />
   </div>
 );
