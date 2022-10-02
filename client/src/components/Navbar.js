e650f5e0d8be90bce4091ed879da02aec9551b89
Mika Bibas <mikabibas@gmail.com>
2022-10-02 10:32:37 +0300

    navbar update

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,21 +6,21 @@ function Navbar({ showTopNavMenu }) {
   const showNav = {
     display: showTopNavMenu ? "flex" : "none",
   };
-  // const showMenuIcon = {
-  //   display: showTopNavMenu ? "none" : "flex",
-  // };
+  const showMenuIcon = {
+    display: showTopNavMenu ? "none" : "flex",
+  };
   return (
     <div className={styles.navContainer} style={showNav}>
-      <li className={styles.listItem}>
+      <li className={styles.listItem} style={showMenuIcon}>
         <Link to="/">WorkItOut</Link>
       </li>
-      <li className={styles.listItem}>
+      <li className={styles.listItem} style={showMenuIcon}>
         <Link to="/plan">My Plans</Link>
       </li>
-      <li className={styles.listItem}>
+      <li className={styles.listItem} style={showMenuIcon}>
         <Link to="/profile">My Profile</Link>
       </li>
-      <li className={styles.listItem}>
+      <li className={styles.listItem} style={showMenuIcon}>
         <Link to="/logout">Logout</Link>
       </li>
     </div>
